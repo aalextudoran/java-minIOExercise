f69e0f2f3c19db0a084526d4ae228e343da146db
aalextudoran <92596716+aalextudoran@users.noreply.github.com>
2024-07-02 02:25:31 +0300

    Update SearchLogService.java

--- src/main/java/com/spa/minioexercise/service/SearchLogService.java
+++ src/main/java/com/spa/minioexercise/service/SearchLogService.java
@@ -4,7 +4,6 @@ import com.spa.minioexercise.entity.SearchLogs;
 import com.spa.minioexercise.dao.SearchLogsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.security.util.Debug;
 
 @Service
 public class SearchLogService {
@@ -15,6 +14,5 @@ public class SearchLogService {
     public void saveSearchLog(String searchTerm) {
         SearchLogs searchLog = new SearchLogs(searchTerm);
         searchLogsRepository.save(searchLog);
-        //Debug.println("debug ","am ajuns aici");
     }
 }
